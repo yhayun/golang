c00d692dadbc8a8b8f28ce7164a46e0ec412aef8
yhayun <yhayun@apple.com>
2017-11-20 20:21:11 +0200

    Source work

--- work/src/mpeg2ts/Source.go
+++ work/src/mpeg2ts/Source.go
@@ -3,8 +3,10 @@ package main
 import (
 	"net"
 	"fmt"
+	"time"
 )
-const UDP_SIZE = 1500;
+const UDP_SIZE = 1500
+const MPEG2TS_PACKET_LENGTH = 188
 
 //server code:   //extractstrean() code
 func main() {
@@ -27,3 +29,40 @@ func main() {
 }
 
 
+
+type  Mpeg2TSource struct {
+	tsPacket Mpeg2TSPacket
+	programPID int // = 0
+	endFlag bool //= false;
+	detectFlag bool //= false;
+	previousTime int64 // = -1
+}
+
+
+
+func (src Mpeg2TSource) extractStream(packet []byte) {
+	for offset:=0; offset < len(packet); offset += MPEG2TS_PACKET_LENGTH {
+		src.tsPacket.fromBytes(packet,offset, src.programPID)
+		//if(src.detectFlag && ((System.currentTimeMillis() - src.previousTime) > 1000)) {
+
+
+
+	}
+}
+
+
+
+
+
+//for (int offset = 0; offset < packet.getLength(); offset += MPEG2TS_PACKET_LENGTH) {
+//
+//tsPacket.fromBytes(packet.getData(), offset, programPID);
+//
+//if(previousTime != -1) {
+//if(detectFlag && ((System.currentTimeMillis() - previousTime) > 1000)) {
+//log.info("Timeout detected, now looking for new PID");
+//programPID = 0;
+//pmtFrame = new PMTFrame();
+//detectFlag = false;
+//}
+//}
