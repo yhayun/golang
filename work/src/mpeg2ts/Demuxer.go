7e779369c2c166a01a1d75329708a7763fea4960
yhayun <yhayun@apple.com>
2017-12-17 19:28:10 +0200

    demuxer so far

--- work/src/mpeg2ts/Demuxer.go
+++ work/src/mpeg2ts/Demuxer.go
@@ -1,12 +1,27 @@
 package main
 
+
+type Unit struct {
+	data []byte
+	Type byte
+	State int
+}
+
+
+type Samples struct {
+	units []Unit
+	pts int
+	dts int
+}
+
+
 // todo - https://github.com/video-dev/hls.js/blob/ce19344d1bfb025e4d55c5b1521f0c8f02c06b5b/src/demux/tsdemuxer.js
 type VideoTrack struct {
 	id uint
 	pid uint
 	inputTimeScale uint
 	sequenceNumber uint
-	samples []byte
+	samples []Samples
 	len uint
 	dropped uint
 	isAAC bool
@@ -48,20 +63,43 @@ func NewVideoTrack() *VideoTrack {
 }
 
 
-type unit struct {
-
+type TSDemuxer struct {
+	track VideoTrack
 }
 
-func ParseAVCNALu(array []byte, track VideoTrack) unit {
+func NewTSDemuxer() *TSDemuxer {
+	return &TSDemuxer{
+		*NewVideoTrack(),
+	}
+}
+
+func (this *TSDemuxer) GetLastNalUnit() *Unit {
+	// todo - do we need this in our special 1 frame case????
+    // avcSample = this.avcSample, lastUnit;
+    //// try to fallback to previous sample if current one is empty
+    //if (!avcSample || avcSample.units.length === 0) {
+    //  let track = this._avcTrack, samples = track.samples;
+    //  avcSample = samples[samples.length-1];
+    //}
+    //if (avcSample) {
+    //  let units = avcSample.units;
+    //  lastUnit = units[units.length - 1];
+    //}
+    //return lastUnit;
+    return nil
+  }
+
+
+func (this *TSDemuxer) ParseAVCNALu(array []byte, ) []Unit {
 	i  := 0
 	len := len(array)
-	var value byte
-	overflow := 0
-	state := track.naluState
-	lastState := state
-	//units
+	var value byte = -1
+	//overflow := 0
+	state := this.track.naluState
+	//lastState := state
+	var units []Unit
 	//unit
-	//unitType
+	var unitType byte
 	lastUnitStart := -1
 	var lastUnitType byte
 
@@ -79,7 +117,7 @@ func ParseAVCNALu(array []byte, track VideoTrack) unit {
       i++
       // optimization. state 0 and 1 are the predominant case. let's handle them outside of the switch/case
       if (state == 0) {
-      	if (value != 0) {
+      	if (value != -1) {
       		state = 0
       		continue
 		}
@@ -87,7 +125,7 @@ func ParseAVCNALu(array []byte, track VideoTrack) unit {
         continue;
       }
       if (state == 1) {
-		  if (value != 0) {
+		  if (value != -1) {
 			  state = 0
 			  continue
 		  }
@@ -96,44 +134,43 @@ func ParseAVCNALu(array []byte, track VideoTrack) unit {
       }
 
       // here we have state either equal to 2 or 3
-      if(!value) {
+      if(value == -1) {
         state = 3;
-      } else if (value === 1) {
-        if (lastUnitStart >=0) {
-          unit = {data: array.subarray(lastUnitStart, i - state - 1), type: lastUnitType};
-          //logger.log('pushing NALU, type/size:' + unit.type + '/' + unit.data.byteLength);
-          units.push(unit);
+      } else if (value == 1) {
+        if (lastUnitStart >=0 ) {
+          unit := Unit{ array[lastUnitStart:i - state - 1], lastUnitType, 0}
+          units = append(units,unit)
         } else {
           // lastUnitStart is undefined => this is the first start code found in this PES packet
           // first check if start code delimiter is overlapping between 2 PES packets,
           // ie it started in last packet (lastState not zero)
           // and ended at the beginning of this PES packet (i <= 4 - lastState)
-          let lastUnit = this._getLastNalUnit();
-          if (lastUnit) {
-            if(lastState &&  (i <= 4 - lastState)) {
-              // start delimiter overlapping between PES packets
-              // strip start delimiter bytes from the end of last NAL unit
-                // check if lastUnit had a state different from zero
-              if (lastUnit.state) {
-                // strip last bytes
-                lastUnit.data = lastUnit.data.subarray(0,lastUnit.data.byteLength - lastState);
-              }
-            }
-            // If NAL units are not starting right at the beginning of the PES packet, push preceding data into previous NAL unit.
-            overflow  = i - state - 1;
-            if (overflow > 0) {
-              //logger.log('first NALU found with overflow:' + overflow);
-              let tmp = new Uint8Array(lastUnit.data.byteLength + overflow);
-              tmp.set(lastUnit.data, 0);
-              tmp.set(array.subarray(0, overflow), lastUnit.data.byteLength);
-              lastUnit.data = tmp;
-            }
+          lastUnit := this.GetLastNalUnit();
+          if (lastUnit != nil) {
+			  // todo - do we need this in our special 1 frame case????
+            //if(lastState != -1 &&  (i <= 4 - lastState)) {
+            //  // start delimiter overlapping between PES packets
+            //  // strip start delimiter bytes from the end of last NAL unit
+            //    // check if lastUnit had a state different from zero
+            //  if (lastUnit.state != -1) {
+            //    // strip last bytes
+            //    lastUnit.data = lastUnit.data.subarray(0,lastUnit.data.byteLength - lastState);
+            //  }
+            //}
+            //// If NAL units are not starting right at the beginning of the PES packet, push preceding data into previous NAL unit.
+            //overflow  = i - state - 1;
+            //if (overflow > 0) {
+            //  //logger.log('first NALU found with overflow:' + overflow);
+            //  let tmp = new Uint8Array(lastUnit.data.byteLength + overflow);
+            //  tmp.set(lastUnit.data, 0);
+            //  tmp.set(array.subarray(0, overflow), lastUnit.data.byteLength);
+            //  lastUnit.data = tmp;
+            //}
           }
         }
         // check if we can read unit type
         if (i < len) {
           unitType = array[i] & 0x1f;
-          //logger.log('find NALU @ offset:' + i + ',type:' + unitType);
           lastUnitStart = i;
           lastUnitType = unitType;
           state = 0;
@@ -146,24 +183,27 @@ func ParseAVCNALu(array []byte, track VideoTrack) unit {
       }
     }
     if (lastUnitStart >=0 && state >=0) {
-      unit = {data: array.subarray(lastUnitStart, len), type: lastUnitType, state : state};
-      units.push(unit);
-      //logger.log('pushing NALU, type/size/state:' + unit.type + '/' + unit.data.byteLength + '/' + state);
+      unit := Unit{ array[lastUnitStart:len], lastUnitType, state}
+      units = append(units,unit)
     }
     // no NALu found
-    if (units.length === 0) {
-      // append pes.data to previous NAL unit
-      let  lastUnit = this._getLastNalUnit();
-      if (lastUnit) {
-        let tmp = new Uint8Array(lastUnit.data.byteLength + array.byteLength);
-        tmp.set(lastUnit.data, 0);
-        tmp.set(array, lastUnit.data.byteLength);
-        lastUnit.data = tmp;
-      }
+    lenUnits := 0
+	for range units {
+		lenUnits++
+	}
+    if (lenUnits == 0) {
+		// todo - do we need this in our special 1 frame case????
+      //// append pes.data to previous NAL unit
+      //let  lastUnit = this._getLastNalUnit();
+      //if (lastUnit) {
+      //  let tmp = new Uint8Array(lastUnit.data.byteLength + array.byteLength);
+      //  tmp.set(lastUnit.data, 0);
+      //  tmp.set(array, lastUnit.data.byteLength);
+      //  lastUnit.data = tmp;
+      //}
     }
-    track.naluState = state;
+    this.track.naluState = state;
     return units;
-
 }
 
 
