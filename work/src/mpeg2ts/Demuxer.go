7f41d2f3cfaec2a3c99a8770a084ca7e89d1f45d
dror barak <dror.barakk@gmail.com>
2018-01-06 16:42:13 +0200

    unversioned

--- work/src/mpeg2ts/Demuxer.go
+++ work/src/mpeg2ts/Demuxer.go
@@ -1,219 +1,219 @@
-package main
-
-
-type Unit struct {
-	data []byte
-	Type byte
-	State int
-}
-
-
-type Sample struct {
-	units []Unit
-	pts float64
-	dts float64
-	length int
-}
-
-type Config struct {
-	width int
-	height int
-	pixelRatio [2]uint
-}
-
-
-// todo - https://github.com/video-dev/hls.js/blob/ce19344d1bfb025e4d55c5b1521f0c8f02c06b5b/src/demux/tsdemuxer.js
-type VideoTrack struct {
-	id uint
-	pid uint
-	inputTimeScale uint
-	timescale int
-	sequenceNumber uint
-	samples []Sample
-	len uint
-	dropped uint
-	isAAC bool
-	duration bool
-
-	//For the first frame also:
-	width int
-	height int
-	sps int//(GOP header)
-	pps int //(I frame header)
-
-	//added during ParseAVCNALu:
-	naluState int
-}
-
-func NewVideoTrack() *VideoTrack {
-	return &VideoTrack{
-		id: 0,
-		pid : -1,
-		inputTimeScale : 90000,
-		sequenceNumber: 0,
-		//samples : [],
-		len : 0,
-		dropped: 0, // type === 'video' ? 0 : undefined,
-		isAAC: false, //type === 'audio' ? true : undefined,
-		duration: false, //type === 'audio' ? duration : undefined
-
-
-		//First Frame only:
-		width: -1,
-		height: -1,
-		sps: -1,
-		pps: -1,
-
-
-		//added during ParseAVCNALu:
-		naluState: 0,
-	}
-}
-
-
-type TSDemuxer struct {
-	track VideoTrack
-}
-
-func NewTSDemuxer() *TSDemuxer {
-	return &TSDemuxer{
-		*NewVideoTrack(),
-	}
-}
-
-func (this *TSDemuxer) GetLastNalUnit() *Unit {
-	// todo - do we need this in our special 1 frame case????
-    // avcSample = this.avcSample, lastUnit;
-    //// try to fallback to previous sample if current one is empty
-    //if (!avcSample || avcSample.units.length === 0) {
-    //  let track = this._avcTrack, samples = track.samples;
-    //  avcSample = samples[samples.length-1];
-    //}
-    //if (avcSample) {
-    //  let units = avcSample.units;
-    //  lastUnit = units[units.length - 1];
-    //}
-    //return lastUnit;
-    return nil
-  }
-
-
-func (this *TSDemuxer) ParseAVCNALu(array []byte, ) []Unit {
-	i  := 0
-	len := len(array)
-	var value byte = -1
-	//overflow := 0
-	state := this.track.naluState
-	//lastState := state
-	var units []Unit
-	//unit
-	var unitType byte
-	lastUnitStart := -1
-	var lastUnitType byte
-
-    if (state == -1) {
-    // special use case where we found 3 or 4-byte start codes exactly at the end of previous PES packet
-      lastUnitStart = 0;
-      // NALu type is value read from offset 0
-      lastUnitType = array[0] & 0x1f;
-      state = 0;
-      i = 1;
-    }
-
-    for (i < len) {
-      value = array[i];
-      i++
-      // optimization. state 0 and 1 are the predominant case. let's handle them outside of the switch/case
-      if (state == 0) {
-      	if (value != -1) {
-      		state = 0
-      		continue
-		}
-        state = 1
-        continue;
-      }
-      if (state == 1) {
-		  if (value != -1) {
-			  state = 0
-			  continue
-		  }
-        state =  2
-        continue
-      }
-
-      // here we have state either equal to 2 or 3
-      if(value == -1) {
-        state = 3;
-      } else if (value == 1) {
-        if (lastUnitStart >=0 ) {
-          unit := Unit{ array[lastUnitStart:i - state - 1], lastUnitType, 0}
-          units = append(units,unit)
-        } else {
-          // lastUnitStart is undefined => this is the first start code found in this PES packet
-          // first check if start code delimiter is overlapping between 2 PES packets,
-          // ie it started in last packet (lastState not zero)
-          // and ended at the beginning of this PES packet (i <= 4 - lastState)
-          lastUnit := this.GetLastNalUnit();
-          if (lastUnit != nil) {
-			  // todo - do we need this in our special 1 frame case????
-            //if(lastState != -1 &&  (i <= 4 - lastState)) {
-            //  // start delimiter overlapping between PES packets
-            //  // strip start delimiter bytes from the end of last NAL unit
-            //    // check if lastUnit had a state different from zero
-            //  if (lastUnit.state != -1) {
-            //    // strip last bytes
-            //    lastUnit.data = lastUnit.data.subarray(0,lastUnit.data.byteLength - lastState);
-            //  }
-            //}
-            //// If NAL units are not starting right at the beginning of the PES packet, push preceding data into previous NAL unit.
-            //overflow  = i - state - 1;
-            //if (overflow > 0) {
-            //  //logger.log('first NALU found with overflow:' + overflow);
-            //  let tmp = new Uint8Array(lastUnit.data.byteLength + overflow);
-            //  tmp.set(lastUnit.data, 0);
-            //  tmp.set(array.subarray(0, overflow), lastUnit.data.byteLength);
-            //  lastUnit.data = tmp;
-            //}
-          }
-        }
-        // check if we can read unit type
-        if (i < len) {
-          unitType = array[i] & 0x1f;
-          lastUnitStart = i;
-          lastUnitType = unitType;
-          state = 0;
-        } else {
-          // not enough byte to read unit type. let's read it on next PES parsing
-          state = -1;
-        }
-      } else {
-        state = 0;
-      }
-    }
-    if (lastUnitStart >=0 && state >=0) {
-      unit := Unit{ array[lastUnitStart:len], lastUnitType, state}
-      units = append(units,unit)
-    }
-    // no NALu found
-    lenUnits := 0
-	for range units {
-		lenUnits++
-	}
-    if (lenUnits == 0) {
-		// todo - do we need this in our special 1 frame case????
-      //// append pes.data to previous NAL unit
-      //let  lastUnit = this._getLastNalUnit();
-      //if (lastUnit) {
-      //  let tmp = new Uint8Array(lastUnit.data.byteLength + array.byteLength);
-      //  tmp.set(lastUnit.data, 0);
-      //  tmp.set(array, lastUnit.data.byteLength);
-      //  lastUnit.data = tmp;
-      //}
-    }
-    this.track.naluState = state;
-    return units;
-}
-
-
-
-
+ package main
+//
+//
+//type Unit struct {
+//	data []byte
+//	Type byte
+//	State int
+//}
+//
+//
+//type Sample struct {
+//	units []Unit
+//	pts float64
+//	dts float64
+//	length int
+//}
+//
+//type Config struct {
+//	width int
+//	height int
+//	pixelRatio [2]uint
+//}
+//
+//
+//// todo - https://github.com/video-dev/hls.js/blob/ce19344d1bfb025e4d55c5b1521f0c8f02c06b5b/src/demux/tsdemuxer.js
+//type VideoTrack struct {
+//	id uint
+//	pid int
+//	inputTimeScale uint
+//	timescale int
+//	sequenceNumber uint
+//	samples []Sample
+//	len uint
+//	dropped uint
+//	isAAC bool
+//	duration bool
+//
+//	//For the first frame also:
+//	width int
+//	height int
+//	sps int//(GOP header)
+//	pps int //(I frame header)
+//
+//	//added during ParseAVCNALu:
+//	naluState int
+//}
+//
+//func NewVideoTrack() *VideoTrack {
+//	return &VideoTrack{
+//		id: 0,
+//		pid : -1,
+//		inputTimeScale : 90000,
+//		sequenceNumber: 0,
+//		//samples : [],
+//		len : 0,
+//		dropped: 0, // type === 'video' ? 0 : undefined,
+//		isAAC: false, //type === 'audio' ? true : undefined,
+//		duration: false, //type === 'audio' ? duration : undefined
+//
+//
+//		//First Frame only:
+//		width: -1,
+//		height: -1,
+//		sps: -1,
+//		pps: -1,
+//
+//
+//		//added during ParseAVCNALu:
+//		naluState: 0,
+//	}
+//}
+//
+//
+//type TSDemuxer struct {
+//	track VideoTrack
+//}
+//
+//func NewTSDemuxer() *TSDemuxer {
+//	return &TSDemuxer{
+//		*NewVideoTrack(),
+//	}
+//}
+//
+//func (this *TSDemuxer) GetLastNalUnit() *Unit {
+//	// todo - do we need this in our special 1 frame case????
+//    // avcSample = this.avcSample, lastUnit;
+//    //// try to fallback to previous sample if current one is empty
+//    //if (!avcSample || avcSample.units.length === 0) {
+//    //  let track = this._avcTrack, samples = track.samples;
+//    //  avcSample = samples[samples.length-1];
+//    //}
+//    //if (avcSample) {
+//    //  let units = avcSample.units;
+//    //  lastUnit = units[units.length - 1];
+//    //}
+//    //return lastUnit;
+//    return nil
+//  }
+//
+//
+//func (this *TSDemuxer) ParseAVCNALu(array []byte, ) []Unit {
+//	i  := 0
+//	len := len(array)
+//	var value byte = -1
+//	//overflow := 0
+//	state := this.track.naluState
+//	//lastState := state
+//	var units []Unit
+//	//unit
+//	var unitType byte
+//	lastUnitStart := -1
+//	var lastUnitType byte
+//
+//    if (state == -1) {
+//    // special use case where we found 3 or 4-byte start codes exactly at the end of previous PES packet
+//      lastUnitStart = 0;
+//      // NALu type is value read from offset 0
+//      lastUnitType = array[0] & 0x1f;
+//      state = 0;
+//      i = 1;
+//    }
+//
+//    for (i < len) {
+//      value = array[i];
+//      i++
+//      // optimization. state 0 and 1 are the predominant case. let's handle them outside of the switch/case
+//      if (state == 0) {
+//      	if (value != -1) {
+//      		state = 0
+//      		continue
+//		}
+//        state = 1
+//        continue;
+//      }
+//      if (state == 1) {
+//		  if (value != -1) {
+//			  state = 0
+//			  continue
+//		  }
+//        state =  2
+//        continue
+//      }
+//
+//      // here we have state either equal to 2 or 3
+//      if(value == -1) {
+//        state = 3;
+//      } else if (value == 1) {
+//        if (lastUnitStart >=0 ) {
+//          unit := Unit{ array[lastUnitStart:i - state - 1], lastUnitType, 0}
+//          units = append(units,unit)
+//        } else {
+//          // lastUnitStart is undefined => this is the first start code found in this PES packet
+//          // first check if start code delimiter is overlapping between 2 PES packets,
+//          // ie it started in last packet (lastState not zero)
+//          // and ended at the beginning of this PES packet (i <= 4 - lastState)
+//          lastUnit := this.GetLastNalUnit();
+//          if (lastUnit != nil) {
+//			  // todo - do we need this in our special 1 frame case????
+//            //if(lastState != -1 &&  (i <= 4 - lastState)) {
+//            //  // start delimiter overlapping between PES packets
+//            //  // strip start delimiter bytes from the end of last NAL unit
+//            //    // check if lastUnit had a state different from zero
+//            //  if (lastUnit.state != -1) {
+//            //    // strip last bytes
+//            //    lastUnit.data = lastUnit.data.subarray(0,lastUnit.data.byteLength - lastState);
+//            //  }
+//            //}
+//            //// If NAL units are not starting right at the beginning of the PES packet, push preceding data into previous NAL unit.
+//            //overflow  = i - state - 1;
+//            //if (overflow > 0) {
+//            //  //logger.log('first NALU found with overflow:' + overflow);
+//            //  let tmp = new Uint8Array(lastUnit.data.byteLength + overflow);
+//            //  tmp.set(lastUnit.data, 0);
+//            //  tmp.set(array.subarray(0, overflow), lastUnit.data.byteLength);
+//            //  lastUnit.data = tmp;
+//            //}
+//          }
+//        }
+//        // check if we can read unit type
+//        if (i < len) {
+//          unitType = array[i] & 0x1f;
+//          lastUnitStart = i;
+//          lastUnitType = unitType;
+//          state = 0;
+//        } else {
+//          // not enough byte to read unit type. let's read it on next PES parsing
+//          state = -1;
+//        }
+//      } else {
+//        state = 0;
+//      }
+//    }
+//    if (lastUnitStart >=0 && state >=0) {
+//      unit := Unit{ array[lastUnitStart:len], lastUnitType, state}
+//      units = append(units,unit)
+//    }
+//    // no NALu found
+//    lenUnits := 0
+//	for range units {
+//		lenUnits++
+//	}
+//    if (lenUnits == 0) {
+//		// todo - do we need this in our special 1 frame case????
+//      //// append pes.data to previous NAL unit
+//      //let  lastUnit = this._getLastNalUnit();
+//      //if (lastUnit) {
+//      //  let tmp = new Uint8Array(lastUnit.data.byteLength + array.byteLength);
+//      //  tmp.set(lastUnit.data, 0);
+//      //  tmp.set(array, lastUnit.data.byteLength);
+//      //  lastUnit.data = tmp;
+//      //}
+//    }
+//    this.track.naluState = state;
+//    return units;
+//}
+//
+//
+//
+//
---
+++ work/src/mpeg2ts/Mpeg2TSSource.go
@@ -0,0 +1,320 @@
+package main
+
+import (
+	"net"
+	"fmt"
+	"time"
+	"io/ioutil"
+)
+const UDP_SIZE = 1500
+const FRAME_SIZE = 5000000; // 5MB
+const MPEG2TS_PACKET_LENGTH = 188
+const MAX_UDP_PACKET_SIZE = 1500;
+
+
+func timeMilisecs() long {
+	return long(time.Now().UnixNano()%1e6/1e3)
+}
+
+type DatagramPacket struct {
+	buf []byte
+	length int
+}
+
+func NewDatagramPacket (b []byte, l int) *DatagramPacket {return &DatagramPacket{b, l}}
+func NewDatagramPacketE () *DatagramPacket {return &DatagramPacket{}}
+
+func (dgPacket* DatagramPacket) GetData() []byte{
+	return dgPacket.buf
+}
+
+func (dgPacket* DatagramPacket) GetLength() int{
+	return dgPacket.length
+}
+
+func (dgPacket* DatagramPacket) SetData(data []byte) {
+	dgPacket.buf = data
+}
+
+type Mpeg2TSSource struct {
+	//socket DatagramSocket
+	videoFrames frameQueue
+    programPID int
+	socket net.UDPConn
+	//outputPort int
+	//socketReceiveBufferSize int
+}
+
+
+//
+//type  UdpSource struct {
+//	tsPacket Mpeg2TSPacket
+//	programPID int // = 0
+//	endFlag bool //= false;
+//	detectFlag bool //= false;
+//	previousTime int64 // = -1
+//	pmtFrame PMTFrame //
+//}
+type UdpSource struct {
+	Mpeg2TSSource
+
+	//outputSocket int //DatagramSocket
+
+	endFlag bool
+	detectFlag bool
+	videoPID int
+
+	videoTSParser Mpeg2TSParser
+	tsPacket Mpeg2TSPacket
+
+	pmtFrame PMTFrame
+	previousTime long
+	streamsMap map[int]StreamInfo //HashMap<Integer, StreamInfo>
+	packet DatagramPacket
+
+}
+
+func (u* UdpSource) extractStreams(packet DatagramPacket) {
+	for offset := 0; offset < packet.GetLength(); offset += MPEG2TS_PACKET_LENGTH {
+
+		u.tsPacket.FromBytes(packet.GetData(), offset, u.programPID);
+
+		if u.previousTime != -1 {
+			if(u.detectFlag && ((timeMilisecs() - u.previousTime) > 1000)) {
+				//log.info("Timeout detected, now looking for new PID");
+				u.programPID = 0;
+				u.pmtFrame = *NewPMTFrame();
+				u.detectFlag = false;
+			}
+		}
+
+		if (u.detectFlag) {
+			if (u.tsPacket.GetPID() == u.videoPID) {
+				u.previousTime = timeMilisecs();
+				u.videoTSParser.Write(u.tsPacket);
+			}
+		} else {
+			if (u.tsPacket.GetType() == Mpeg2TSPacketType_PAT) {
+				fmt.Println("PAT found")
+				u.programPID = u.tsPacket.GetProgramPID();
+			}
+
+			if (u.tsPacket.GetType() == Mpeg2TSPacketType_PMT) { // PMT received
+				fmt.Println("PMT received")
+				if (u.pmtFrame.AddPacket(u.tsPacket)) {// pmtFrame is complete
+
+					u.streamsMap = u.pmtFrame.GetStreamInfos();
+
+					//for (map.Entry<Integer, StreamInfo> entry : streamsMap
+					//.entrySet()) {
+					for _, v := range u.streamsMap {
+						var info StreamInfo = v
+						if (info.streamType == TS_STREAM_TYPE_H264) {
+							fmt.Println("New PID detected = ", info.streamPID)
+							u.previousTime = timeMilisecs()
+							u.detectFlag = true;
+							u.videoPID = info.streamPID;
+							break;
+						}
+
+					}
+				}
+			}
+
+		}
+	}
+}
+
+func (u* UdpSource) FrameQueueFiller() {
+	var firstPacket bool = true
+	var buffer= make([]byte, 1500)
+	u.packet.SetData(buffer)
+	fmt.Println("Entered FrameQueueFiller")
+
+	for u.endFlag != true {
+		rlen, _, err := u.socket.ReadFromUDP(buffer)
+		if err != nil {
+			fmt.Println("No packet received")
+			continue
+		}
+		if firstPacket {
+			fmt.Println("At least one packet was received")
+			firstPacket = false
+		}
+		var pgPacket = NewDatagramPacket(buffer, rlen)
+		//fmt.Println(buffer[:30])
+		u.extractStreams(*pgPacket)
+	}
+	fmt.Println("exited loop")
+	defer u.socket.Close();
+}
+
+
+func NewMpeg2TSSource(port int,queue frameQueue) *Mpeg2TSSource {
+	addr, _ := net.ResolveUDPAddr("udp", ":8888")
+	sock, _ := net.ListenUDP("udp", addr)
+	return &Mpeg2TSSource{queue, 0, *sock}
+}
+func NewUdpSource(port int,queue frameQueue) * UdpSource{
+	return &UdpSource{
+		*NewMpeg2TSSource(port,queue),
+		false,
+		false,
+		0,
+		*NewMpeg2TSParser(queue),
+		*NewMpeg2TSPacket(),
+		*NewPMTFrame(),
+		-1,
+		make(map[int]StreamInfo),
+		*NewDatagramPacketE(),
+	}
+}
+
+
+//-----------------------------------------------------------//
+// tester.go file code moved here
+//-----------------------------------------------------------//
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func WriteFile(buffer []byte, file string){
+	err := ioutil.WriteFile(file, buffer, 0644)
+	check(err)
+}
+
+func CheckIfIFrame(buffer []byte, offset int, length int) bool {
+	var startOffset int = offset
+	for i := startOffset; i < (length -7); i++ {
+		var j int = offset + i
+		if buffer[j] == 0 {
+			if buffer[j+1] == 0 {
+				if buffer[j+2] == 1 {
+					var val short= (short) (0x001f & buffer[j + 3])
+					if val == 7 {
+						return true // Start GOP was detected
+					}
+				}
+			}
+		}
+	}
+	return false
+}
+
+func FrameQueueDispatcherFullFile(videoFrames frameQueue) {
+	fmt.Println("entered FrameQueueDispatcher")
+	h264Buffer := make([]byte, 60*1024*1024)
+	var length int = 0
+	var iframeDetected bool = false
+	for {
+		//fmt.Println("entered consmer loop")
+		if videoFrames.IsEmpty() {
+			fmt.Println("consume sleep 100ms")
+			time.Sleep(100 * time.Millisecond)
+			if videoFrames.IsEmpty() {
+				fmt.Println("consume sleep 8000ms")
+				time.Sleep(8000 * time.Millisecond)
+				if videoFrames.IsEmpty() {
+					break
+				}
+			}
+		}
+		var frame *Frame  = videoFrames.Poll();
+		if !iframeDetected {
+			if CheckIfIFrame(frame.GetData(),0, frame.Size()) {
+				iframeDetected = true
+			} else {
+				videoFrames.Recylce(frame)
+				continue
+			}
+		}
+		ArrayCopy(frame.GetData(),0, h264Buffer,length,frame.Size())
+		length += frame.Size()
+		videoFrames.Recylce(frame)
+		if (length >= 22500000){
+			break;
+		}
+	}
+	fmt.Println("left FrameQueueDispatcher loop.")
+	WriteFile(h264Buffer[:22500000],"tempfile1.264");
+	Done <- true
+}
+
+
+
+
+func FrameQueueDispatcher(videoFrames frameQueue) {
+	fmt.Println("entered FrameQueueDispatcher")
+	//Used to identify frames when written to files.
+	var counter = 0
+	var i_counter = 0
+	var p_counter = 0
+	var p_type = 'P'
+	var i_type = 'I'
+	var s string //= fmt.Sprintf("../tmp/frame[%d]-I[%d]-P[%d]_TYPE<%c>", counter,i_counter,p_counter,p_type)
+	var iframeDetected bool = false
+
+	for {
+		//fmt.Println("entered consmer loop")
+		if videoFrames.IsEmpty() {
+			fmt.Println("consume sleep 100ms")
+			time.Sleep(100 * time.Millisecond)
+			if videoFrames.IsEmpty() {
+				fmt.Println("consume sleep 8000ms")
+				time.Sleep(8000 * time.Millisecond)
+				if videoFrames.IsEmpty() {
+					break
+				}
+			}
+		}
+		var frame *Frame  = videoFrames.Poll();
+		if !iframeDetected {
+			if CheckIfIFrame(frame.GetData(),0, frame.Size()) {
+				iframeDetected = true
+			} else {
+				videoFrames.Recylce(frame)
+				continue
+			}
+		}
+
+		counter++
+		if CheckIfIFrame(frame.GetData(),0, frame.Size()) {
+			i_counter++
+			p_counter = 0
+			s = fmt.Sprintf("../../tmp/frame[%d]-I[%d]-P[%d]_TYPE<%c>__size:%d", counter,i_counter,p_counter,i_type,frame.Size())
+		} else {
+			p_counter++;
+			s = fmt.Sprintf("../../tmp/frame[%d]-I[%d]-P[%d]_TYPE<%c>", counter,i_counter,p_counter,p_type)
+
+		}
+
+		WriteFile(frame.GetData(),s)
+		videoFrames.Recylce(frame)
+		if (counter >= 500){
+			break;
+		}
+
+	}
+	fmt.Println("left FrameQueueDispatcher loop.")
+	Done <- true
+}
+
+
+
+
+
+//Used to force main thread to go to sleep so we can handle when program stops running.
+var Done = make(chan bool)
+
+func main() {
+	var videoFrames frameQueue = *NewFrameQueue(100,FRAME_SIZE)
+	//var tsSource Mpeg2TSSource = *NewMpeg2TSSource(8888, videoFrames)
+	var uSource UdpSource = *NewUdpSource(100, videoFrames)
+	fmt.Println("working on UDP");
+	go uSource.FrameQueueFiller()
+	go FrameQueueDispatcher(videoFrames)
+	<-Done
+}
